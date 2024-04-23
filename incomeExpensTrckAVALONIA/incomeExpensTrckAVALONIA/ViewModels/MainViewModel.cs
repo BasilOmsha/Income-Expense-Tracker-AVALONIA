611e667e7eda1ff1fea3aa9a45dea3b6ce5b33e5
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-04-23 18:00:06 +0300

    Deleted for re-initiation

--- incomeExpensTrckAVALONIA/incomeExpensTrckAVALONIA/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace incomeExpensTrckAVALONIA.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- incomeExpensTrckAVALONIA/incomeExpensTrckAVALONIA/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace incomeExpensTrckAVALONIA.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
